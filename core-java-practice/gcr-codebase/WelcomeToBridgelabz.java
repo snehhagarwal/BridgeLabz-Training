53b11484339a11d02835afa752018534d648a1fd
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-17 15:49:13 +0530

    [Sneha Agarwal]Add. Program to print Welcome to BridgeLabz

---
+++ core-java-practice/gcr-codebase/WelcomeToBridgelabz.java
@@ -0,0 +1,7 @@
+
+public class WelcomeToBridgelabz {
+    public static void main(String[] args) {     
+        //printing the statement on the screen
+        System.out.println("Welcome to BridgeLabz!");
+    }
+}
