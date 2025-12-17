b3028bf71648eca8055421f551d5c0f96a8925d2
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-17 15:10:54 +0530

    Code to check whether the string is palindrome or not

---
+++ core-java-practice/leet-code-codebase/PalindromeString.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //Enter the string 
+        String str=sc.nextLine();
+
+        System.out.println(func(str));
+    }
+
+    public static boolean func(String s){
+        //solved using two pointer approach
+        int left=0;  
+        int right=s.length()-1;
+
+        //condition to break the loop 
+        while(left<right){
+            //checks the letters from both the ends of the string
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;  //increase the starting pointer
+            right--; //decrease the ending pointer
+        }
+        //if all the letters are same then the string is palindrome
+        return true;
+    }
+}
+
