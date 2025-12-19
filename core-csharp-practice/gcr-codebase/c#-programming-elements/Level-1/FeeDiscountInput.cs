802c0dd4889457804028f42b89a56ea8fdc4b0ae
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-19 15:58:51 +0530

    [Sneha Agarwal] Add . Code to calculate the amount of fee to be paid after discount

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/Level-1/FeeDiscountInput.cs
@@ -0,0 +1,21 @@
+using System;
+
+class FeeDiscountInput{
+    static void Main(){
+
+        //input from user
+        Console.WriteLine("Ener the fee: ");
+        double fee=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the discount percentage: ");
+        double discountperc=Convert.ToDouble(Console.ReadLine());
+        
+        //dicount calculation
+        double discount=(fee*discountperc)/100;
+        //final to be paid
+        double final=fee - discount;
+
+        Console.WriteLine("The discount amount is INR " + discount + " and the final discounted fee is INR " + final);
+        
+    }
+}
