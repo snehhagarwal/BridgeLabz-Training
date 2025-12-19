816585e3acc61a76fd3a376f8e1f8d72c0243bf0
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-19 16:28:34 +0530

    [Sneha Agarwal] Add . Code to calculate the maximum number of handshakes

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/Level-1/Handshakes.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Handshakes{
+    static void Main(){
+
+        //taking as input
+        Console.WriteLine("Enter no of students: ");
+
+        int n=Convert.ToInt32(Console.ReadLine());
+
+        //calculating the handshakes
+        int handshake=(n*(n-1))/2;
+
+        Console.WriteLine("Maximum number of handshakes is " + handshake);
+        
+
+    }
+}
