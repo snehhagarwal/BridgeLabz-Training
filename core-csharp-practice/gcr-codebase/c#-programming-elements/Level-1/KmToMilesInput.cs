9726fbf480cf0fa466297b485ec03ca916b32f6b
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-19 15:53:58 +0530

    [Sneha Agarwal] Add . Code to convert the distance from km to miles

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/Level-1/KmToMilesInput.cs
@@ -0,0 +1,15 @@
+using System;
+
+class KmToMilesInput{
+    static void Main(){
+ 
+        //input from user
+        Console.WriteLine("Enter the distance in km: ");
+        double km=Convert.ToDouble(Console.ReadLine());
+
+        //convert in miles
+        double miles=km/1.6;
+
+        Console.WriteLine("The total miles is " + miles + " mile for the given " + km + " km");
+    }
+}
