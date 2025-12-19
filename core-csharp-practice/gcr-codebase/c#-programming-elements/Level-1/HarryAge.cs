4293536e8dd2dbed5ca0f83dbd27cf434f7cb420
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-19 15:25:11 +0530

    [Sneha Agarwal] Add . Code to calculate the age of harry

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/Level-1/HarryAge.cs
@@ -0,0 +1,14 @@
+using System;
+
+class HarryAge{
+   static void Main(string[] args){
+      
+      int birthyear=2000;
+      int currentyear=2024;
+
+      //age of harry by subtracting the years
+      int age=currentyear-birthyear;
+      Console.Write("Harry's age in 2024 is " + age);
+
+   }
+}
