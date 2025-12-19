8a19eff4f495d24b9e27661dfe6d873ade4fb660
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-19 12:17:36 +0530

    [Sneha Agarwal] Add . Code to perform all the operators

--- core-java-practice/gcr-codebase/WelcomeToBridgelabz.java
+++
@@ -1,7 +0,0 @@
-
-public class WelcomeToBridgelabz {
-    public static void main(String[] args) {     
-        //printing the statement on the screen
-        System.out.println("Welcome to BridgeLabz!");
-    }
-}
