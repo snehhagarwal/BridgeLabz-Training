4991bf1fa0c84c823cbada451df26d6004d3545d
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-12-25 22:52:21 +0530

    [Sneha Agarwal] Add . Leetcode Solutions

---
+++ core-csharp-practice/leet-code-codebase/LongestSubstring.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class LongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int i=0;
+        int j=0;
+        int max=0;
+        HashMap<Character,Integer> mp=new HashMap<>();
+        while(j<s.length()){
+            if(mp.containsKey(s.charAt(j))){
+                i=Math.max(i,mp.get(s.charAt(j))+1);
+            }
+            mp.put(s.charAt(j),j);
+            max=Math.max(j-i+1,max);
+            j++;
+        }
+        return max;
+    }
+}
