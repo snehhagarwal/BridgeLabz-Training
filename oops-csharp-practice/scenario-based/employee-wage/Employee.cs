6175bcf88e0121351608b39378bdd2e0db7dc5c4
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2026-01-07 16:31:28 +0530

    [Sneha Agarwal] Add . Code to add Employee class with properties

--- oops-csharp-practice/scenario-based/employee-wage/Employee.cs
+++ oops-csharp-practice/scenario-based/employee-wage/Employee.cs
@@ -9,26 +9,18 @@ namespace BridgeLabz_Training.employee_wage
     internal class Employee
     {
         // Stores employee ID
-        private int empId;
+        private int EmployeeId { get; set; }
 
         // Stores employee name
-        private string empName;
+        private string EmployeeName { get; set; }
 
         // Stores employee salary
-        private double dailyWage;
-
-        //setting values using getters and setters 
-        public int EmployeeId { get=>empId; set => empId = value; }
-
-        public string EmployeeName { get=>empName; set => empName = value; }
-
-        public double DailyWage { get => dailyWage; set => dailyWage = value; }
-
+        private double EmployeeSalary { get; set; }
 
         // Returns employee details as string
         public override string ToString()
         {
-            return "Employee ID: " + EmployeeId + ", Employee Name: " + EmployeeName + ", Employee Salary: " + DailyWage;
+            return "Employee ID: " + EmployeeId + ", Employee Name: " + EmployeeName + ", Employee Salary: " + EmployeeSalary;
         }
     }
 }
