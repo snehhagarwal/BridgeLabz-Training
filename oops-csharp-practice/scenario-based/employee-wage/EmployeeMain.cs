b615d938eb797750c4c2f2357f4143b048b39d08
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2026-01-07 15:55:44 +0530

    [Sneha Agarwal] Add . UC0:- Start message display

---
+++ oops-csharp-practice/scenario-based/employee-wage/EmployeeMain.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training.employee_wage
+{
+    internal class EmployeeMain
+    {
+        private EmployeeMenu employeeMenu;
+        public static void Main(string[] args)
+        {
+            // UC0: Display welcome message
+            Console.WriteLine("Welcome to Employee Wage Program");
+
+            // Creating EmployeeMenu object
+            EmployeeMenu employeeMenu =new EmployeeMenu();
+
+            // Calling menu to execute UC1–UC6 operations
+            employeeMenu.EmployeeMenuChoices();
+        }
+    }
+}
