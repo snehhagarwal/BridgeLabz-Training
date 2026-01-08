077235681592b904af0b0cec557e5d5d4fd28986
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2026-01-08 21:00:00 +0530

    [Sneha Agarwal] Add . Codes to solve questions on linked list

--- dsa-csharp-practice/gcr-codebase/linked-list/RoundRobinScheduling.cs
+++ dsa-csharp-practice/gcr-codebase/linked-list/RoundRobinScheduling.cs
@@ -3,8 +3,11 @@ using System;
 // Represents a process node in the circular linked list
 class ProcessNode
 {
-    // Process ID and burst time
     public int Id,Burst;
+    public int RemainingTime;
+    public int Priority;
+    public int WaitingTime;
+    public int TurnAroundTime;
     // Pointer to next node
     public ProcessNode next;
 }
@@ -16,9 +19,9 @@ class RoundRobin
     ProcessNode head;
 
     // Adds a new process to the list
-    public void Add(int id,int burst)
+    public void Add(int id,int burst,int priority)
     {
-        ProcessNode node=new ProcessNode{Id=id,Burst=burst};
+        ProcessNode node=new ProcessNode{Id=id,Burst=burst,RemainingTime=burst,Priority=priority};
         if (head == null)
         {
             head=node;
@@ -35,6 +38,44 @@ class RoundRobin
         node.next=head;
     }
 
+    //Remove process by ID
+    private void RemoveProcess(int id)
+    {
+        if (head == null)
+        {
+            return;
+        }
+
+        if(head.Id==id && head.next == head)
+        {
+            head=null;
+            return;
+        }
+
+        if (head.Id == id)
+        {
+            ProcessNode temp=head;
+            while (temp.next != head)
+            {
+                temp=temp.next;
+            }
+            temp.next=head.next;
+            head=head.next;
+            return;
+        }
+
+        ProcessNode curr=head;
+        while(curr.next!=head && curr.next.Id != id)
+        {
+            curr=curr.next;
+        }
+
+        if (curr.next.Id == id)
+        {
+            curr.next=curr.next.next;
+        }
+    }
+
     // Displays all processes in the list
     public void Display()
     {
@@ -45,9 +86,55 @@ class RoundRobin
         ProcessNode temp=head;
         do
         {
-            Console.WriteLine(temp.Id + " BT:- " + temp.Burst);
+            Console.WriteLine(temp.Id + " BT:- " + temp.Burst + " Remaining: " + temp.RemainingTime);
             temp=temp.next;
         }while(temp!=head);
+    }
+
+    public void Schedule(int timeQuantum)
+    {
+        int time=0;
+        int completed=0;
+        int totalWaiting=0;
+        int totalTurnAround=0;
+
+        ProcessNode curr=head;
+
+        while (head != null)
+        {
+            if (curr.RemainingTime > 0)
+            {
+                Console.WriteLine("Executing Process: " + curr.Id);
+                if (curr.RemainingTime > timeQuantum)
+                {
+                    curr.RemainingTime-=timeQuantum;
+                    time+=timeQuantum;
+                }
+                else
+                {
+                    time+=curr.RemainingTime;
+                    curr.RemainingTime=0;
+                    curr.TurnAroundTime=time;
+                    curr.WaitingTime=curr.TurnAroundTime-curr.Burst;
+                    totalWaiting+=curr.WaitingTime;
+                    totalTurnAround+=curr.TurnAroundTime;
+                    completed++;
+
+                    int completedId=curr.Id;
+                    curr=curr.next;
+                    RemoveProcess(completedId);
+
+                    Display();
+                    continue;
+                }
+            }
+            curr=curr.next;
+            Display();
+        }
+
+        Console.WriteLine("Average Waiing Time: " + (double)totalWaiting/completed);
+        
+        Console.WriteLine("Average TurnAround Time: " + (double)totalTurnAround/completed);
     }
 }
 
@@ -58,8 +145,16 @@ class RRMain
     static void Main(string[] args)
     {
         RoundRobin rr=new RoundRobin();
-        rr.Add(1,5);
-        rr.Add(2,3);
+        rr.Add(1,5,1);
+        rr.Add(2,3,2);
+        rr.Add(3,8,1);
+
+        int timeQuantum=2;
+
+        Console.WriteLine("Initial Process: ");
         rr.Display();
+
+        Console.WriteLine("Round Robin Scheduling: ");
+        rr.Schedule(timeQuantum);
     }
 }
