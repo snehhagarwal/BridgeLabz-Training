e6d9bd1e413b34e27f27c098b297d1a4d2e41b1a
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2026-01-16 16:17:24 +0530

    [Sneha Agarwal] Add. Codes to solve scenario based questions

---
+++ dsa-csharp-practice/scenario-based/traffic-manager/Vehicle.cs
@@ -0,0 +1,12 @@
+namespace traffic_manager
+{
+    class Vehicle
+    {
+        public string Number { get; private set; }
+
+        public Vehicle(string number)
+        {
+            Number = number;
+        }
+    }
+}
