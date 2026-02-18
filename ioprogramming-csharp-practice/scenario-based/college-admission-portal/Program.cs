9257883518f2795bc05ddb1a19fd5cb8b110c27f
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2026-02-18 16:02:55 +0530

    [Sneha Agarwal] Add. Implementation of college admission portal

---
+++ ioprogramming-csharp-practice/scenario-based/college-admission-portal/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        RegistrationService service = new RegistrationService();
+
+        string[] emails =
+        {
+            "john.doe@gmail.com",
+            "megha_r92@outlook.in",
+            "admin@blitz.edu",
+            "john.doe@gmail",
+            "@gmail.com",
+            "raju#123@inbox.com"
+        };
+
+        foreach (var email in emails)
+        {
+            Student student = new Student(email);
+            service.Register(student);
+        }
+
+        Console.ReadLine();
+    }
+}
