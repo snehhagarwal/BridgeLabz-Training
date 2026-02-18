dfbc454f700a6e14d3f9dbb5a0ec2cc13432f661
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2026-02-18 16:47:24 +0530

    [Sneha Agarwal] Add . UC-12 ability to read or write the address book with person contact into file

--- generics-csharp-practice/scenario-based/address-book/AddressBookMain.cs
+++ generics-csharp-practice/scenario-based/address-book/AddressBookMain.cs
@@ -1,21 +1,77 @@
 using System;
+class AddressBookMain{
+    static void Main(string[] args){
+        AddressBookUtility utility = new AddressBookUtility();
+        utility.DisplayWelcomeMessage();
+        int choice;
+        do{
+            Console.WriteLine("\n---- MENU ----");
+            Console.WriteLine("1. Create Address Book");
+            Console.WriteLine("2. Select Address Book");
+            Console.WriteLine("3. Add Contact");
+            Console.WriteLine("4. Add Multiple Contacts");
+            Console.WriteLine("5. Edit Contact");
+            Console.WriteLine("6. Delete Contact");
+            Console.WriteLine("7. Search Person By City");
+            Console.WriteLine("8. Search Person By State");
+            Console.WriteLine("9. View Person By City");
+            Console.WriteLine("10. View Person By State");
+            Console.WriteLine("11. Count Person By City");
+            Console.WriteLine("12. Count Person By State");
+            Console.WriteLine("13. Sort Contacts by Name");
+            Console.WriteLine("14. Sort Contacts by City");
+            Console.WriteLine("15. Sort Contacts by State");
+            Console.WriteLine("16. Sort Contacts by Zip");
+            Console.WriteLine("17. Write Contacts to File");
+            Console.WriteLine("18. Read Contacts from File");
+            Console.WriteLine("19. Exit");
+            Console.Write("Enter choice: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice){
+                case 1:
+                    AddressBookUtility.CreateAddressBook();
+                    break;
+                case 2:
+                    utility = AddressBookUtility.SelectAddressBook();
+                    break;
+                case 3:
+                    utility.AddContact();
+                    break;
+                case 4:
+                    utility.AddMultipleContacts();
+                    break;
+                case 5:
+                    utility.EditContact();
+                    break;
+                case 6:
+                    utility.DeleteContact();
+                    break;
+                case 13:
+                    utility.SortContactsByName();
+                    break;
+                case 14:
+                    utility.SortContactsByCity();
+                    break;
+                case 15:
+                    utility.SortContactsByState();
+                    break;
+                case 16:
+                    utility.SortContactsByZip();
+                    break;
+                case 17:
+                    utility.WriteContactsToFile();
+                    break;
+                case 18:
+                    utility.ReadContactsFromFile();
+                    break;
+                case 19:
+                    Console.WriteLine("Exiting Program.");
+                    break;
+                default:
+                    Console.WriteLine("Inavlid choice.Try again Later");
+                    break;
+            }
 
-namespace AddressBookSystem
-{
-    internal class AddressBookMain
-    {
-        public static void Main(string[] args)
-        {
-            try
-            {
-                Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM ---> ");
-                AddressBookMenu menu = new AddressBookMenu();
-                menu.ShowMenu();
-            }
-            catch (Exception ex) // catches unexpected crash
-            {
-                Console.WriteLine("System Error: " + ex.Message);
-            }
-        }
+        } while (choice != 19);
     }
 }
--- generics-csharp-practice/scenario-based/address-book/IAddressBook.cs
+++ generics-csharp-practice/scenario-based/address-book/IAddressBook.cs
@@ -1,28 +1,11 @@
-namespace AddressBookSystem{
-    internal interface IAddressBook
-    {
-            void AddContact();     // UC2 + UC7
-            void EditContact();    // UC3
-            void DeleteContact();  // UC4
-            void AddMultipleContacts(); // UC5
+public interface IAddressBook{
+    void DisplayWelcomeMessage(); // UC-0
+    void AddContact();            // UC-1 + UC-2
+    void EditContact();           // UC-3
+    void DeleteContact();         // UC-4
+    void AddMultipleContacts();   // UC-5
+    void SortContactsByCity();    //UC-12
+    void SortContactsByState();   //UC-12
+    void SortContactsByZip();    //UC-12
 
-            void SearchByCityOrState();   // UC8
-            void ViewPersonsByCityOrState();   // UC9
-            void CountPersonsByCityOrState();   // UC10
-
-            void SortContactsByName();   // UC-11
-
-            void SortContactsByCity();  //UC-12
-            void SortContactsByState();  //UC-12
-            void SortContactsByZip();    //UC-12
-
-
-            void WriteContactsToFile();  //UC-13
-            void ReadContactsFromFile();  //UC-13
-
-
-
-    }
 }
-
-
